c29ebf498b9d3ec7c36da823fb3fc7f6fa13060a
Kzsanjeet <127291186+Kzsanjeet@users.noreply.github.com>
2023-11-27 22:32:48 +0545

    added practiced about adjecentHtml

---
+++ DOM manupulation/adjacent.js
@@ -0,0 +1,10 @@
+// //loads before brgining of div or element
+// first.insertAdjacentHTML('beforebegin','<div class="test">beforebegin</div>')
+// //loads before end of the div or element
+// first.insertAdjacentHTML('beforeend','<div class="test">beforeend</div>')
+// //loads after the begining of the element
+// first.insertAdjacentHTML('afterbegin','<div class="test">afterbegin</div>')
+// //loads after the end of the element
+// first.insertAdjacentHTML('afterend','<div class="test">afterend</div>')
+
+first.remove()  
